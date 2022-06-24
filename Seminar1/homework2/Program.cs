fe48992c20a44e9a6b194eff568c2a2231fc23dc
Aleksa Bosi <aleksabosi@gmail.com>
2022-06-24 23:47:39 +0300

    add Semenar 1 homework 2

---
+++ Seminar1/homework2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void getMaxNumber()
+{
+    Console.Write("Введите число: ");
+    int temp = Convert.ToInt32(Console.ReadLine());
+    number = number < temp ? temp : number;
+}
+
+getMaxNumber();
+getMaxNumber();
+
+Console.WriteLine($"Наибольшее число = {number}");
