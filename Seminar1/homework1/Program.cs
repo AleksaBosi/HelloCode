8202eb5e323856a08a7f46126f2bb46cba680470
Aleksa Bosi <aleksabosi@gmail.com>
2022-06-24 23:52:37 +0300

    add Semenar 1 homework 1

---
+++ Seminar1/homework1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number > number2) Console.Write(number);
+else Console.Write(number2);
