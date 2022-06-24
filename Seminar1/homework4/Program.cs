ada412e5174d14ce59ee204ae8811232713feed9
Aleksa Bosi <aleksabosi@gmail.com>
2022-06-25 00:16:33 +0300

    add Semenar 1 homework 4

---
+++ Seminar1/homework4/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= number; i += 2) if (number == i) Console.Write(i);
+    else Console.Write($"{i}, ");
